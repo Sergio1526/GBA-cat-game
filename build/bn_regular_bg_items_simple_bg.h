55ef541b469085244df54d377219adc643cbd6cc
Sergio1526 <sergioaosma@gmail.com>
2024-07-09 08:44:05 -0500

    Added collisions maps

--- build/bn_regular_bg_items_simple_bg.h
+++ build/bn_regular_bg_items_simple_bg.h
@@ -7,13 +7,13 @@
 
 //======================================================================
 //
-//	simple_bg_bn_gfx, 256x256@8, 
+//	simple_bg_bn_gfx, 256x256@4, 
 //	+ palette 16 entries, not compressed
-//	+ 99 tiles (t|f reduced) lz77 compressed
+//	+ 99 tiles (t|f|p reduced) not compressed
 //	+ regular map (flat), not compressed, 32x32 
-//	Total size: 32 + 944 + 2048 = 3024
+//	Total size: 32 + 3168 + 2048 = 5248
 //
-//	Time-stamp: 2024-06-25, 22:16:12
+//	Time-stamp: 2024-07-08, 21:19:59
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -22,8 +22,8 @@
 #ifndef GRIT_SIMPLE_BG_BN_GFX_H
 #define GRIT_SIMPLE_BG_BN_GFX_H
 
-#define simple_bg_bn_gfxTilesLen 944
-extern const bn::tile simple_bg_bn_gfxTiles[198];
+#define simple_bg_bn_gfxTilesLen 3168
+extern const bn::tile simple_bg_bn_gfxTiles[99];
 
 #define simple_bg_bn_gfxMapLen 2048
 extern const bn::regular_bg_map_cell simple_bg_bn_gfxMap[1024];
@@ -38,8 +38,8 @@ extern const bn::color simple_bg_bn_gfxPal[16];
 namespace bn::regular_bg_items
 {
     constexpr inline regular_bg_item simple_bg(
-            regular_bg_tiles_item(span<const tile>(simple_bg_bn_gfxTiles, 198), bpp_mode::BPP_8, compression_type::LZ77), 
-            bg_palette_item(span<const color>(simple_bg_bn_gfxPal, 16), bpp_mode::BPP_8, compression_type::NONE),
+            regular_bg_tiles_item(span<const tile>(simple_bg_bn_gfxTiles, 99), bpp_mode::BPP_4, compression_type::NONE), 
+            bg_palette_item(span<const color>(simple_bg_bn_gfxPal, 16), bpp_mode::BPP_4, compression_type::NONE),
             regular_bg_map_item(simple_bg_bn_gfxMap[0], size(32, 32), compression_type::NONE, 1, false));
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@
 // Code
 namespace
 {
-    bn::point cat_map_position(0, 0);
+    bn::point cat_map_position(16, 16);
 
     void logo_scene(bn::camera_ptr& camera)
     {
@@ -86,6 +86,9 @@ namespace
 
         //Backgrounds
         bn::regular_bg_ptr simple_bg = bn::regular_bg_items::simple_bg.create_bg(0, 0);
+        //Generate map for collisions
+        const bn::regular_bg_map_item& map_item = bn::regular_bg_items::simple_bg.map_item();
+
         bn::regular_bg_ptr clouds_bg = bn::regular_bg_items::clouds.create_bg(0, 0);
         bn::blending::set_transparency_alpha(0.1);
         clouds_bg.set_blending_enabled(true);
@@ -155,6 +158,10 @@ namespace
                 cat_map_position.set_x(cat_map_position.x() + 1);
                 cat_sprite.set_horizontal_flip(false);
                 BN_LOG("Map pos: ", " X:", cat_map_position.x(), " Y:", cat_map_position.y());
+                //Check collider
+                bn::regular_bg_map_cell player_map_cell = map_item.cell(cat_map_position);
+                int player_tile_index = bn::regular_bg_map_cell_info(player_map_cell).tile_index();
+                BN_LOG("Player cell: ", player_tile_index);
             }
 
             if (bn::keypad::up_held())
